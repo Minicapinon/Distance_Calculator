cb4716098773e8dec7a60b7aceb559c3f7761acf
Minicapinon <bava080502hgtrtna6@cbtis217.edu.mx>
2024-10-14 15:24:26 +0200

    Added code to 'calculation.cpp' and fix sintaxys to 'Main.cpp'

--- Main.cpp
+++ Main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char const *argv[])
     std::cin>>y1;std::cout<<std::endl;
     std::cin>>y2;std::cout<<std::endl;
 
-    double res = Operation(x1,x2,y1,y2);
+    double res = calculation::operation(x1,x2,y1,y2);
 
     std::cout<<res;
     return 0;
--- calculation.cpp
+++ calculation.cpp
@@ -1,6 +1,13 @@
 #ifndef CALCULATION
 #include <cmath>
 
+double operation(double x1,double x2,double y1,double y2)
+{
+    double distance = 0;
 
+    distance = sqrt((x2-x1)*(x2-x1)+(y2-y1)(y2-y1));
+
+    return distance;
+}
 
 #endif //CALCULATION
